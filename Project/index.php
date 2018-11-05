b6742cd88dfba49d440639294623ac7e31ee2ea0
James Willhoite <jameswillhoite@gmail.com>
2018-11-05 11:22:24 -0500

    Changing the Customer Info from Input fields to span that will be generated from the Select Customer Modal

--- Project/index.php
+++ Project/index.php
@@ -20,6 +20,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="libraries/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="libraries/bootstrap/datepicker/css/bootstrap-datepicker3.min.css">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/project.css">
     <link rel="stylesheet" href="css/project.responsive.css">
     <link rel="stylesheet" href="libraries/fontawesome/css/all.css">
@@ -56,7 +57,7 @@
     <div class="clearfix"></div>
 
     <div class="row">
-        <div id="error_msg" class="col-12"></div>
+        <div id="error_msg" class="col-12 alert"></div>
     </div>
 
     <div id="main" class="container">
@@ -94,7 +95,7 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-3 col-sm-3 col-xs-4">
                         <label for="customerName">Customer Name</label>
-                        <input type="text" id="customerName" class="form-control" pattern="[a-zA-Z\s\.]{5,}" autocomplete="off" required />
+                        <span id="customerName"></span>
                     </div>
                 </div>
                 <div class="row">
@@ -187,6 +188,104 @@
 
         </div>
     </div>
+
+    <!-- Add Customer Modal -->
+    <div id="addCustomerModal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add New Customer</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-7 col-xs-12">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><span class="input-group-text" id="fullNameLabel">Full Name</span> </div>
+                                <input type="text" id="fullName" class="form-control" autocomplete="off">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-7 col-xs-12 mt-1">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><span class="input-group-text">Address</span> </div>
+                                <input type="text" id="address" class="form-control" autocomplete="off">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><span class="input-group-text">City</span> </div>
+                                <input type="text" id="city" class="form-control" autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><span class="input-group-text">ST</span> </div>
+                                <input type="text" id="state" class="form-control" value="OH" autocomplete="off">
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><span class="input-group-text">Zip</span> </div>
+                                <input type="number" id="zip" class="form-control no-spinner" pattern="/d*" autocomplete="off">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><select id="phoneType" class="form-control">
+                                        <option value="cell">Cell</option>
+                                        <option value="home">Home</option>
+                                        <option value="work">Work</option>
+                                    </select>
+                                </div>
+                                <input type="tel" id="phone" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-1">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <div class="input-group">
+                                <div class="input-group-prepend"><span class="input-group-text">E-Mail</span> </div>
+                                <input type="email" id="email" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Select Customer -->
+    <div id="selectCustomerModal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Select Customer</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <button id="scrollToTop" title="Go to top"><span class="fas fa-caret-up"></span> </button>
 </div>
 
@@ -198,6 +297,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="libraries/bootstrap/datepicker/js/bootstrap-datepicker.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script src="js/quote.js"></script>
 <script>
     jQuery(document).ready(function() {
